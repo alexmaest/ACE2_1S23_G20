8f2eaa6fc935e6f2a548afe6f9cfab4e52915a65
rodri <3065713820401@ingenieria.usac.edu.gt>
2023-02-08 11:44:42 -0600

    add relative humidity p5 component

--- Practica 1/frontend/pages/components/HumedadRelativa.js
+++ Practica 1/frontend/pages/components/HumedadRelativa.js
@@ -1,22 +1,22 @@
 import dynamic from 'next/dynamic'
 import Link from 'next/link'
 
-// importamos react-p5 en client-side
-// por defecto NextJs usa SSR (Server Side Rendering)
-
 const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
   ssr: false,
 })
 
 let cBack, cFront
-let movTemp = 0
+
 const sizeWith = 250
 const sizeHeight = 250
 
 const x = sizeWith / 2
-const y = sizeHeight / 1.3
+const y = sizeHeight / 2
 
-export default function Temperatura() {
+let radiusBack = 200
+let radiusFront = 50
+
+export default function HumedadRelativa() {
   const setup = (p5, canvasParentRef) => {
     // use parent to render the canvas in this ref
     // (without that p5 will render the canvas outside of your component)
@@ -24,27 +24,21 @@ export default function Temperatura() {
   }
 
   const draw = (p5) => {
-    // Crear un termometro
     p5.noStroke()
     p5.background('#0f172a')
-    // Se crea un objeto de tipo color
-    cFront = p5.color(239, 68, 68)
-    cBack = p5.color(209, 213, 219)
-    // Se asigna el color al objeto
+    cBack = p5.color(250, 204, 21)
+    cFront = p5.color(37, 99, 235)
     p5.fill(cBack)
-    // Se dibuja un rectangulo de la base del termometro
-    p5.rect(x - 10, y - 150, 20, 120)
+    p5.ellipse(x, y, radiusBack, radiusBack)
     p5.fill(cFront)
-    p5.rect(x - 5, y - movTemp, 10, movTemp)
-    // Se asigna el color al objeto
-    p5.fill(cBack)
-    // Se dibuja un círculo de la base del termometro
-    p5.ellipse(x, y, 70, 70)
-    p5.fill(cFront)
-    p5.ellipse(x, y, 55, 55)
+    p5.ellipse(
+      x,
+      y + radiusBack / 2 - radiusFront / 2,
+      radiusFront,
+      radiusFront
+    )
 
-    movTemp >= 150 ? (movTemp = 0) : movTemp++
-
+    radiusFront >= 200 ? (radiusFront = 0) : radiusFront++
     // NOTE: Do not use setState in the draw function or in functions that are executed
     // in the draw function...
     // please use normal variables or class properties for these purposes
@@ -53,7 +47,7 @@ export default function Temperatura() {
   return (
     <div className="w-64 flex-row content-center">
       <div className="text-white bg-cyan-800 text-center mb-2 rounded mx-10">
-        Temperatura externa
+        Humedad Relativa
       </div>
       <Link href="/temperatura">
         <div className="mx-[3px]">
---
+++ Practica 1/frontend/pages/temperatura.js
@@ -0,0 +1,10 @@
+
+function temperaturaPage() {
+    return (
+        <div className='text-white'>
+        <h1>Grafica temperatura</h1>
+        </div>
+    );
+}
+
+export default temperaturaPage
