b25a93539c5c26eeece36e6d6aa9d82e444ee015
David Maldonado <dvidmaldo@gmail.com>
2023-02-08 13:08:53 -0600

    Update serialport
    Update serialport nodejs

--- Practica 1/apiProcessing/src/index.js
+++ Practica 1/apiProcessing/src/index.js
@@ -1,7 +1,8 @@
 const express = require('express'); // express sirve para crear el servidor
 const cors = require('cors'); // cors sirve para permitir el acceso a la API desde cualquier origen
 const morgan = require('morgan'); // morgan sirve para ver las peticiones que se hacen a la API
-const serialPort = require('serialport'); // serialport sirve para comunicarse con el puerto serial`
+const SerialPort = require('serialport').SerialPort;
+const { DelimiterParser } = require('@serialport/parser-delimiter');
 require('./db/config.js'); // se importa el archivo de configuracion de la base de datos
 
 const app = express();
@@ -9,8 +10,8 @@ const port = 3001;
 
 const corsOptions = { origin: true, OptionsSuccessStatus: 200 }; // origin: true permite el acceso desde cualquier origen y OptionsSuccessStatus: 200 es para que el navegador no muestre un error
 
-const usbPort = new serialPort('COM3', { baudRate: 9600 }); // se crea un puerto serial con el puerto COM3 y una velocidad de 9600 baudios
-const parser = new serialPort.parsers.Readline(); // se crea un parser para leer los datos que se envian por el puerto serial
+const usbPort = new SerialPort({ path: 'COM4', baudRate: 9600 });
+const parser = usbPort.pipe(new DelimiterParser({ delimiter: '\n' }));
 
 app.use(cors(corsOptions)); // se usa cors con las opciones que se definieron arriba
 app.use(express.json({ extended: true })); // se usa express.json para que el servidor entienda los datos que se envian en formato json y extended: true es para que el servidor entienda datos mas complejos;
@@ -23,9 +24,17 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-usbPort.pipe(parser); // se usa el parser para leer los datos que se envian por el puerto serial
-
-parser.on('data', (data) => { // se crea un evento para cuando se reciban datos por el puerto serial
-  console.log(data); // se imprime en consola los datos que se reciben por el puerto serial
+parser.on('open', () => {
+  console.log('Serial Port Opened');
 });
 
+parser.on('data', (data) => {
+  var decoder = new TextDecoder();
+  var arr = new Uint8Array(data);
+  info = decoder.decode(arr);
+  console.log(info);
+});
+
+parser.on('error', (err) => {
+  console.log(err);
+});
