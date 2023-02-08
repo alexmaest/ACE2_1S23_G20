20bb78facf953072a8b18d4fb8d44a837636463d
David Maldonado <dvidmaldo@gmail.com>
2023-02-08 10:23:34 -0600

    Serial communication
    Serial communication between backend and arduino

--- Practica 1/Arduino/Arduino.ino
+++ Practica 1/Arduino/Arduino.ino
@@ -1,3 +1,6 @@
+#include <math.h>
+#include <string.h>
+
 #include <Wire.h>//Incluye libreria de bus I2C
 #include <Adafruit_Sensor.h>//Incluye librerias para sensor BMP280
 #include <Adafruit_BMP280.h>
@@ -6,12 +9,17 @@
 #include <DHT_U.h>
 
 #define Type DHT11
+const float a = 17.625;
+const float b = 243.04;
+const float e = 2.71828;
 int dhtPin = 2;
 int humidity;
 float tempC;
 float tempF;
 float tempC2;
 float pression;
+float dewPoint;
+string data;
 
 DHT HT(2, Type);
 Adafruit_BMP280 bmp;
@@ -29,16 +37,28 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  float alpha;
+  float ln;
   humidity = HT.readHumidity();
   tempC = HT.readTemperature();
   tempF = HT.readTemperature(true);
   tempC2 = bmp.readTemperature();
   pression = (bmp.readPressure()/100);
-  Serial.println("Humedad relativa: " + String(humidity) + "%");
-  Serial.println("Temperatura1 C: " + String(tempC));
-  Serial.println("Temperatura1 F: " + String(tempF));
-  Serial.println("Presion hPa: " + String(pression));
-  //Serial.println("Temperatura2 C: " + String(tempC2));
-  Serial.println("--------------------------------------------------------");
+  ln = log(humidity) / log(e)
+  alpha = ln + ((a * tempC) / (b + tempC));
+  dewPoint = (b * alpha) / (a - alpha);
+  // format data
+  data = "";
+  data += String(tempC);
+  data += ","
+  data += String(humidity);
+  data += ",";
+  // TODO: humedad absoluta
+  data += String(dewPoint);
+  data += ",";
+  // TODO: velocidad del viento
+  // TODO: direccion del viento
+  data += String(pression);
+  Serial.println(data);
   delay(3000);
 }
--- Practica 1/apiProcessing/src/index.js
+++ Practica 1/apiProcessing/src/index.js
@@ -1,12 +1,17 @@
 const express = require('express'); // express sirve para crear el servidor
 const cors = require('cors'); // cors sirve para permitir el acceso a la API desde cualquier origen
 const morgan = require('morgan'); // morgan sirve para ver las peticiones que se hacen a la API
+const serialPort = require('serialport'); // serialport sirve para comunicarse con el puerto serial`
 require('./db/config.js'); // se importa el archivo de configuracion de la base de datos
 
 const app = express();
 const port = 3001;
 
 const corsOptions = { origin: true, OptionsSuccessStatus: 200 }; // origin: true permite el acceso desde cualquier origen y OptionsSuccessStatus: 200 es para que el navegador no muestre un error
+
+const usbPort = new serialPort('COM3', { baudRate: 9600 }); // se crea un puerto serial con el puerto COM3 y una velocidad de 9600 baudios
+const parser = new serialPort.parsers.Readline(); // se crea un parser para leer los datos que se envian por el puerto serial
+
 app.use(cors(corsOptions)); // se usa cors con las opciones que se definieron arriba
 app.use(express.json({ extended: true })); // se usa express.json para que el servidor entienda los datos que se envian en formato json y extended: true es para que el servidor entienda datos mas complejos;
 app.use(express.urlencoded({ extended: true })); // se usa express.urlencoded para que el servidor entienda los datos que se envian en formato urlencoded y extended: true es para que el servidor entienda datos mas complejos;
@@ -18,6 +23,9 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
+usbPort.pipe(parser); // se usa el parser para leer los datos que se envian por el puerto serial
 
-
+parser.on('data', (data) => { // se crea un evento para cuando se reciban datos por el puerto serial
+  console.log(data); // se imprime en consola los datos que se reciben por el puerto serial
+});
 
