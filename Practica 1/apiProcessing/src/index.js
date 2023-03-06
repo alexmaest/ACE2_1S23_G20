0356de5c466f4a48bc10201283a886d194c1b726
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-03-06 09:25:51 -0600

    add frontend

--- Practica 1/apiProcessing/src/index.js
+++ Practica 1/apiProcessing/src/index.js
@@ -1,33 +1,33 @@
-const express = require('express'); // express sirve para crear el servidor
-const cors = require('cors'); // cors sirve para permitir el acceso a la API desde cualquier origen
-const morgan = require('morgan'); // morgan sirve para ver las peticiones que se hacen a la API
-const SerialPort = require('serialport').SerialPort;
-const { DelimiterParser } = require('@serialport/parser-delimiter');
-require('./db/config.js'); // se importa el archivo de configuracion de la base de datos
-const fetch = require('node-fetch');
+const express = require('express') // express sirve para crear el servidor
+const cors = require('cors') // cors sirve para permitir el acceso a la API desde cualquier origen
+const morgan = require('morgan') // morgan sirve para ver las peticiones que se hacen a la API
+/* const SerialPort = require('serialport').SerialPort;
+const { DelimiterParser } = require('@serialport/parser-delimiter'); */
+require('./db/config.js') // se importa el archivo de configuracion de la base de datos
+const fetch = require('node-fetch')
 
-const app = express();
-const port = 3001;
+const app = express()
+const port = 3001
 //
-const corsOptions = { origin: true, OptionsSuccessStatus: 200 }; // origin: true permite el acceso desde cualquier origen y OptionsSuccessStatus: 200 es para que el navegador no muestre un error
+const corsOptions = { origin: true, OptionsSuccessStatus: 200 } // origin: true permite el acceso desde cualquier origen y OptionsSuccessStatus: 200 es para que el navegador no muestre un error
 //
-const usbPort = new SerialPort({ path: 'COM4', baudRate: 9600 });
-const parser = usbPort.pipe(new DelimiterParser({ delimiter: '\n' }));
+/* const usbPort = new SerialPort({ path: 'COM4', baudRate: 9600 });
+const parser = usbPort.pipe(new DelimiterParser({ delimiter: '\n' })); */
 
-const directions = { 0: "N", 1: "E", 2: "S", 3: "O" };
+const directions = { 0: 'N', 1: 'E', 2: 'S', 3: 'O' }
 
-app.use(cors(corsOptions)); // se usa cors con las opciones que se definieron arriba
-app.use(express.json({ extended: true })); // se usa express.json para que el servidor entienda los datos que se envian en formato json y extended: true es para que el servidor entienda datos mas complejos;
-app.use(express.urlencoded({ extended: true })); // se usa express.urlencoded para que el servidor entienda los datos que se envian en formato urlencoded y extended: true es para que el servidor entienda datos mas complejos;
-app.use(morgan('dev')); // se usa morgan para ver las peticiones que se hacen a la API
+app.use(cors(corsOptions)) // se usa cors con las opciones que se definieron arriba
+app.use(express.json({ extended: true })) // se usa express.json para que el servidor entienda los datos que se envian en formato json y extended: true es para que el servidor entienda datos mas complejos;
+app.use(express.urlencoded({ extended: true })) // se usa express.urlencoded para que el servidor entienda los datos que se envian en formato urlencoded y extended: true es para que el servidor entienda datos mas complejos;
+app.use(morgan('dev')) // se usa morgan para ver las peticiones que se hacen a la API
 
-app.use('/api', require('./routes/routes.js')); // se usa el archivo de rutas
+app.use('/api', require('./routes/routes.js')) // se usa el archivo de rutas
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
-parser.on('open', () => {
+/* parser.on('open', () => {
   console.log('Serial Port Opened');
 });
 
@@ -57,21 +57,17 @@ parser.on('data', (data) => {
 
 parser.on('error', (err) => {
   console.log(err);
-});
+}); */
 
 //functin para enviar info en ruta /api/enviar
 function enviarInfo(json = {}) {
   fetch('http://localhost:3001/api/enviar', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(json)
+    body: JSON.stringify(json),
   })
-    .then(res => res.json())
-    .then(data => console.log(data));
+    .then((res) => res.json())
+    .then((data) => console.log(data))
 }
-
-
-
-
