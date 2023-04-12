36c4baafd37e14777697fa36c4762930a54de228
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-04-12 10:36:27 -0600

    app send initial configuration to mongoDB

--- Practica 2/app/lib/main.dart
+++ Practica 2/app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MaterialApp(
@@ -15,22 +16,101 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final timeToWaterController = TextEditingController();
+  final backendURL = 'http://10.0.2.2:3001';
+  Map data = {};
+  var settings = {
+    'power': false,
+    'timeToWater': 0,
+  };
+
   getSettings() async {
     http.Response response =
-        await http.get(Uri.parse('http://localhost:3001/api/settings'));
-    debugPrint(response.body);
+        await http.get(Uri.parse('$backendURL/api/settings'));
+    data = json.decode(response.body);
+    setState(() {
+      Map dataSettings = data['settings'][0];
+      settings['power'] = dataSettings['power'];
+      settings['timeToWater'] = dataSettings['timeToWater'];
+      timeToWaterController.text = settings['timeToWater'].toString();
+    });
+    debugPrint(settings.toString());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getSettings();
+  }
+
+  @override
+  void dispose() {
+    timeToWaterController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ACE2'),
-        backgroundColor: Colors.indigo[900],
-      ),
-      body: const Center(
-        child: Text('Grupo 20'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Grupo 20'),
+          backgroundColor: Colors.indigo[900],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Sistema De Riego',
+                  style: TextStyle(fontSize: 24.0),
+                )),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              ElevatedButton(
+                  onPressed: () async {
+                    http.Response response = await http.put(
+                        Uri.parse('$backendURL/api/settings/modify'),
+                        body: json.encode({
+                          "power": true,
+                          "timeToWater":
+                              int.tryParse(timeToWaterController.text) ?? 0
+                        }),
+                        headers: {"Content-Type": "application/json"});
+                    getSettings();
+                    debugPrint(response.body);
+                  },
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.green)),
+                  child: const Text('Encender')),
+              ElevatedButton(
+                  onPressed: () async {
+                    http.Response response = await http.put(
+                        Uri.parse('$backendURL/api/settings/modify'),
+                        body: json.encode({
+                          "power": false,
+                          "timeToWater":
+                              int.tryParse(timeToWaterController.text) ?? 0
+                        }),
+                        headers: {"Content-Type": "application/json"});
+                    getSettings();
+                    debugPrint(response.body);
+                  },
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.red)),
+                  child: const Text('Apagar'))
+            ]),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: timeToWaterController,
+                keyboardType: TextInputType.number,
+                decoration:
+                    const InputDecoration(hintText: 'Tiempo de riego (s)'),
+              ),
+            )
+          ],
+        ));
   }
 }
--- Practica 2/backend/src/routes/settings.js
+++ Practica 2/backend/src/routes/settings.js
@@ -4,14 +4,14 @@ const router = Router()
 const Setting = require('../models/Setting')
 
 router.get('/api/settings', async (req, res) => {
-  const Settings = await Setting.find()
-  res.json(Settings)
+  const settings = await Setting.find()
+  res.json({ settings })
 })
 
-router.post('/api/settings/add', async (req, res) => {
+router.put('/api/settings/modify', async (req, res) => {
   const { power, timeToWater } = req.body
-  await Setting.create({ power, timeToWater })
-  res.json('Setting added')
+  await Setting.findOneAndUpdate({}, { power, timeToWater })
+  res.json({ message: 'Settings updated' })
 })
 
 module.exports = router
