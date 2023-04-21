3c40db81b5bf5774c4828ad9db1f043ab82adf2f
DavidAMaldonadoH <64713060+DavidAMaldonadoH@users.noreply.github.com>
2023-04-21 12:47:32 -0600

    chore: show real time data in frontend

--- Proyecto 2/frontend/src/components/Dashboard.jsx
+++ Proyecto 2/frontend/src/components/Dashboard.jsx
@@ -1,10 +1,60 @@
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { socket } from "../socket";
+import axios from "../api/axios";
 
 import Temperature from "./Temperature";
 import Pie from "./Pie";
 
+const GET_REAL_TIME_DATA = "/api/realTimeData";
+
 function Dashboard() {
+  const [externalTemperature, setExternalTemperature] = useState(0);
+  const [internalTemperature, setInternalTemperature] = useState(0);
+  const [humidity, setHumidity] = useState(0);
+  const [waterPercentage, setWaterPercentage] = useState(0);
   const navigate = useNavigate();
+
+  const getRealTimeData = async () => {
+    try {
+      let { data } = await axios.get(GET_REAL_TIME_DATA);
+      data = data.realTimeData[0];
+      console.log(data);
+      setExternalTemperature(data.externalTemperature);
+      setInternalTemperature(data.internalTemperature);
+      setHumidity(data.soilMoisture);
+      setWaterPercentage(data.waterLevel);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getRealTimeData();
+  }, []);
+
+  useEffect(() => {
+    socket.connect();
+    socket.on("externalTemperature", (data) => {
+      setExternalTemperature(data);
+    });
+    socket.on("internalTemperature", (data) => {
+      setInternalTemperature(data);
+    });
+    socket.on("soilMoisture", (data) => {
+      setHumidity(data);
+    });
+    socket.on("waterLevel", (data) => {
+      setWaterPercentage(data);
+    });
+    return () => {
+      socket.off("externalTemperature");
+      socket.off("internalTemperature");
+      socket.off("soilMoisture");
+      socket.off("waterLevel");
+      socket.disconnect();
+    }
+  }, []);
 
   return (
     <div className="w-full h-full min-h-screen flex items-center bg-gradient-to-l from-sky-500 to-emerald-500 py-10">
@@ -20,7 +70,7 @@ function Dashboard() {
         <div className="grid grid-cols-2 gap-6 font-chivo-mono">
           <div className="backdrop-blur-lg bg-white/30 rounded shadow-2xl basis-1/4 p-6 grid justify-items-center">
             <p className="text-lg">Temperatura Externa</p>
-            <Temperature temp={20} />
+            <Temperature temp={externalTemperature} />
             <button
               className="rounded-full bg-green-400 hover:hover:brightness-105 px-8 py-2 "
               onClick={() => navigate("/external-temperature")}
@@ -28,7 +78,7 @@ function Dashboard() {
           </div>
           <div className="backdrop-blur-lg bg-white/30 rounded shadow-2xl basis-1/4 p-6 grid justify-items-center">
             <p className="text-lg">Temperatura Interna</p>
-            <Temperature temp={35} />
+            <Temperature temp={internalTemperature} />
             <button
               className="rounded-full bg-green-400 hover:hover:brightness-105 px-8 py-2 "
               onClick={() => navigate("/internal-temperature")}
@@ -39,7 +89,7 @@ function Dashboard() {
         <div className="grid grid-cols-2 gap-6 font-chivo-mono">
           <div className="backdrop-blur-lg bg-white/30 rounded shadow-2xl basis-1/4 p-6 grid justify-items-center">
             <p className="text-lg">Humedad de Tierra</p>
-            <Pie percentage={50} />
+            <Pie percentage={humidity} />
             <button
               className="rounded-full bg-green-400 hover:hover:brightness-105 px-8 py-2 "
               onClick={() => navigate("/humidity")}
@@ -47,7 +97,7 @@ function Dashboard() {
           </div>
           <div className="backdrop-blur-lg bg-white/30 rounded shadow-2xl basis-1/4 p-6 grid justify-items-center">
             <p className="text-lg">Porcentaje de Agua</p>
-            <Pie percentage={75} />
+            <Pie percentage={waterPercentage} />
             <button
               className="rounded-full bg-green-400 hover:hover:brightness-105 px-8 py-2 "
               onClick={() => navigate("/water-percentage")}
